afdf7a4999e9c3fc006bc6632fdd259d65c6df09
veldymanov@gmail.com <Andii_Veldymanov@epam.com>
2024-05-23 12:53:43 +0300

    feat: shared packages implemented

--- src/app/auth/login-dialog/login-dialog.component.ts
+++ src/app/auth/login-dialog/login-dialog.component.ts
@@ -1,8 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { AuthService } from '@auth/services/auth.service';
-import { UserLogin } from '@ng-nest-postgre/luv-coffee-be/dist/libs/fe-shared/src';
-// import { UserRole } from '@ng-nest-postgre/luv-coffee-be/dist/libs/fe-shared/src';
+import { UserLogin } from '@angularnestpostgre/packages/fe-shared';
+// import { UserRole } from '@angularnestpostgre/packages/fe-shared';
 
 @Component({
   selector: 'anp-login-dialog',
