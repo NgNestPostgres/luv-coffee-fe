c315f2140ccf1a687cc39ec3ead846c7e6a82bb9
veldymanov@gmail.com <Andii_Veldymanov@epam.com>
2025-03-30 17:51:58 +0300

    add Responsive Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import {MediaMatcher} from '@angular/cdk/layout';
 import {
   ChangeDetectionStrategy,
-  ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild,
+  Component, inject, Signal,
 } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import {MatSidenav, MatSidenavModule} from '@angular/material/sidenav';
+import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {RouterModule, RouterOutlet} from '@angular/router';
+import {ResponsiveService} from '@core/services/responsive.service';
 
 import {SearchBoxComponent} from './navigation/search-box/search-box.component';
 import {SidenavTreeComponent} from './navigation/sidenav-tree/sidenav-tree.component';
@@ -32,41 +32,9 @@ import {TopMenuComponent} from './navigation/top-menu/top-menu.component';
     RouterOutlet,
   ],
 })
-export class AppComponent implements OnInit, OnDestroy {
-  @ViewChild('sidenav') private sidenav!: MatSidenav;
+export class AppComponent {
+  responsicService = inject(ResponsiveService);
 
-  public tabQuery!: MediaQueryList;
-  public mobileQuery!: MediaQueryList;
-
-  private mobileQueryListener = () => this.mobileQueryMatched();
-  private tabQueryListener = () => this.tabQueryMatched();
-
-  constructor(
-    private changeDetectorRef: ChangeDetectorRef,
-    private media: MediaMatcher,
-  ) {}
-
-  ngOnInit(): void {
-    this.mobileQuery = this.media.matchMedia('(max-width: 600px)');
-    this.tabQuery = this.media.matchMedia('(max-width: 1025px)');
-    this.mobileQuery.addEventListener('change', this.mobileQueryListener);
-    this.tabQuery.addEventListener('change', this.tabQueryListener);
-  }
-
-  ngOnDestroy(): void {
-    this.mobileQuery.removeEventListener('change', this.mobileQueryListener);
-    this.mobileQuery.removeEventListener('change', this.tabQueryListener);
-  }
-
-  private mobileQueryMatched(): void {
-    this.changeDetectorRef.detectChanges();
-  }
-
-  private tabQueryMatched(): void {
-    if (!this.tabQuery.matches) {
-      this.sidenav.close();
-    }
-
-    this.changeDetectorRef.detectChanges();
-  }
+  isMobile: Signal<boolean> = this.responsicService.isMobile;
+  isDesktop: Signal<boolean> = this.responsicService.isDesktop;
 }
---
+++ src/app/core/services/responsive.service.ts
@@ -0,0 +1,50 @@
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {computed, inject, Injectable, Signal, signal, WritableSignal} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ResponsiveService {
+  private responsive = inject(BreakpointObserver);
+
+  private isMobileWS: WritableSignal<boolean> = signal(false);
+  private isTabletWS: WritableSignal<boolean> = signal(false);
+  private isDesktopWS: WritableSignal<boolean> = signal(false);
+
+  isMobile: Signal<boolean> = computed(() => this.isMobileWS());
+  isTablet: Signal<boolean> = computed(() => this.isTabletWS()); ;
+  isDesktop: Signal<boolean> = computed(() => this.isDesktopWS()); ;
+
+  constructor() {
+    this.responsive.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+    ])
+      .subscribe((result) => {
+        const breakpoints = result.breakpoints;
+
+        this.isMobileWS.set(false);
+        this.isTabletWS.set(false);
+        this.isDesktopWS.set(false);
+
+        if (breakpoints[Breakpoints.XSmall]) {
+          if (result.matches) {
+            this.isMobileWS.set(true);
+          }
+        }
+
+        if (breakpoints[Breakpoints.Small]) {
+          if (result.matches) {
+            this.isTabletWS.set(true);
+          }
+        }
+
+        if (breakpoints[Breakpoints.Medium]) {
+          if (result.matches) {
+            this.isDesktopWS.set(true);
+          }
+        }
+      });
+  }
+}
