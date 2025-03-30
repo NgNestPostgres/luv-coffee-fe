ef7209c92a72011d57a1be37af97c61d56d7542a
veldymanov@gmail.com <Andii_Veldymanov@epam.com>
2025-03-30 09:27:40 +0300

    refactor HomeComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import {Routes} from '@angular/router';
 
-import {HomeComponent} from './home/home/home.component';
+import {HomeComponent} from './home/home.component';
 import {NotFoundComponent} from './shared/components/not-found/not-found.component';
 
 export const routes: Routes = [
